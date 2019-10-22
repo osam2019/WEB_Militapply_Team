31b855ca5dae95e7d6b5c7774cf017d94097bfa3
identity16 <dnjswns0930@gmail.com>
2019-10-22 22:27:25 +0900

    불필요한 의존성 정리

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import ElementUI from "element-ui";
-import "vuetify/dist/vuetify.min.css";
 import DashBoard from "./components/DashBoard.vue";
 import SearchPage from "./components/SearchPage.vue";
 import Schedule from "./components/Schedule.vue";
