5c4288604ca29eb657b3f30cdef70800b4e35029
identity16 <dnjswns0930@gmail.com>
2019-10-22 22:13:20 +0900

    Fullcalendar 한글 적용

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,36 +1,41 @@
 <template>
-  <FullCalendar defaultView="dayGridMonth" :weekends="false" :plugins="calendarPlugins" :events="events"/>
+  <FullCalendar
+    defaultView="dayGridMonth"
+    :weekends="false"
+    :plugins="calendarPlugins"
+    :events="events"
+    locale="ko"
+  />
 </template>
 
 <script>
-  import FullCalendar from '@fullcalendar/vue';
-  import listPlugin from '@fullcalendar/daygrid';
+import FullCalendar from "@fullcalendar/vue";
+import listPlugin from "@fullcalendar/daygrid";
 
-  export default {
-    name: "Schedule",
-    components: {
-      FullCalendar
+export default {
+  name: "Schedule",
+  components: {
+    FullCalendar
   },
-    data() {
-      return {
-        calendarPlugins: [ listPlugin ],
-        events: require('../../data/events'),
-        footer: {
-          left:   'title',
-          center: '',
-          right:  'today prev,next'
-        }
+  data() {
+    return {
+      calendarPlugins: [listPlugin],
+      events: require("../../data/events"),
+      footer: {
+        left: "title",
+        center: "",
+        right: "today prev,next"
       }
-    }
+    };
   }
-
+};
 </script>
 
 <style scoped>
-  @import '~@fullcalendar/core/main.css';
-  @import '~@fullcalendar/daygrid/main.css';
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/daygrid/main.css";
 
-  .fc-toolbar .fc-header-toolbar {
-    font-size: 12px;
-  }
+.fc-toolbar .fc-header-toolbar {
+  font-size: 12px;
+}
 </style>
