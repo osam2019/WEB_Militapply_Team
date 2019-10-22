f390480fa2564fe43264e5a6a54ec0428f69e428
identity16 <dnjswns0930@gmail.com>
2019-10-22 17:52:07 +0900

    DashBoard에 달력 추가(fullcalendar)

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,9 +1,36 @@
 <template>
-  <div></div>
+  <FullCalendar defaultView="dayGridMonth" :weekends="false" :plugins="calendarPlugins" :events="events"/>
 </template>
 
 <script>
-export default {
-  name: "Schedule"
-};
+  import FullCalendar from '@fullcalendar/vue';
+  import listPlugin from '@fullcalendar/daygrid';
+
+  export default {
+    name: "Schedule",
+    components: {
+      FullCalendar
+  },
+    data() {
+      return {
+        calendarPlugins: [ listPlugin ],
+        events: require('../../data/events'),
+        footer: {
+          left:   'title',
+          center: '',
+          right:  'today prev,next'
+        }
+      }
+    }
+  }
+
 </script>
+
+<style scoped>
+  @import '~@fullcalendar/core/main.css';
+  @import '~@fullcalendar/daygrid/main.css';
+
+  .fc-toolbar .fc-header-toolbar {
+    font-size: 12px;
+  }
+</style>
